da13bf491dbc03dd7c20ef7a0a09d387eda53fc8
Alex Rapley <rapley3@gmail.com>
2017-05-23 11:54:59 +1000

    Add control buttons to timer

--- src/MeditateTimer.js
+++ src/MeditateTimer.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 class MeditateTimer extends Component {
   constructor() {
     super();
+    this.tick = this.tick.bind(this);
+    this.start = this.start.bind(this);
+    this.pause = this.pause.bind(this);
+    this.resume = this.resume.bind(this);
+    this.reset = this.reset.bind(this);
+    // this.tick = this.tick.bind(this);
     this.state = { secondsElapsed: 0 };
   }
 
@@ -12,18 +18,35 @@ class MeditateTimer extends Component {
     }));
   }
 
-  componentDidMount() {
+  start() {
     this.interval = setInterval(() => this.tick(), 1000);
   }
 
-  componentWillMount() {
+  pause() {
     clearInterval(this.interval);
+    delete this.interval;
+  }
+
+  resume() {
+    if (!this.interval) this.start();
+  }
+
+  reset() {
+    this.setState({
+      secondsElapsed: 0
+    })
   }
 
   render() {
     return (
       <div className="widget">
+        <h2>Meditation Timer</h2>
         Seconds Elapsed: {this.state.secondsElapsed}
+        <br />
+        <button onClick={() => this.start()}>Start</button>
+        <button onClick={() => this.pause()}>pause</button>
+        <button onClick={() => this.resume()}>resume</button>
+        <button onClick={() => this.reset()}>reset</button>
       </div>
     );
   }
